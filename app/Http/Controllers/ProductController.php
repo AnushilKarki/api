1a454da56449c9d2d0120737419a9c5f52a1dfac
anushil <anushil.karki34@gmail.com>
2020-06-10 19:43:32 +0545

    creating and using productrequest with resource for creating a product from api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Requests\ProductRequest;
 use App\Product;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 use App\Http\Resource\Product\ProductCollection;
 use App\Http\Resource\Product\ProductResource;
 class ProductController extends Controller
@@ -13,6 +14,10 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
     public function index()
     {
      return ProductCollection::collection(Product::paginate(20));  
@@ -34,9 +39,18 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product=new Product;
+        $product->name=$request->name;
+        $product->detail=$request->description;
+        $product->stock=$request->stock;
+        $product->price=$request->price;
+        $product->discount=$request->discount;
+        $product->save();
+        return response([
+            'data'=>new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
